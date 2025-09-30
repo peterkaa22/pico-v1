6a798f93b1d68711b1e96d7d37ba9fb407e8620e
Adam Peterka <peterkaa22@spstrutnov.cz>
2025-09-30 14:40:28 +0200

    UART- těžší

--- UART.py
+++ UART.py
@@ -5,8 +5,8 @@ uart = UART(0, baudrate=9600, tx=Pin(0), rx=Pin(1))
 button = Pin(15, Pin.IN, Pin.PULL_UP)
 
 while True:
-    if not button.value():  # tlačítko stisknuté (GND, tedy LOW)
-        uart.write(b'1')  # pošli znak '1'
-        print("Tlačítko zmáčknuto, odesílám '1'")
-        time.sleep(0.3)  # debounce + pauza, aby se neposílalo pořád
+    if not button.value():
+        uart.write(b'1')
+        print("Tlačítko zmáčknuto, posílám '1'")
+        time.sleep(0.5)  # debounce
     time.sleep(0.05)
